6d814b7a81bbae7612052f7f9954a236053b2123
Natphipps <phippsna@msu.edu>
2023-08-23 12:27:06 -0400

    Completed station analysis and started creating the climate app

--- SurfsUp/Starter_Code/app.py
+++ SurfsUp/Starter_Code/app.py
@@ -1,28 +1,57 @@
 # Import the dependencies.
-
-
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 #################################################
 # Database Setup
 #################################################
-
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with = engine)
 
 # Save references to each table
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(bind = engine)
 
 #################################################
 # Flask Setup
 #################################################
+from flask import Flask
+
+app = Flask(__name__)
+
+date = {"prcp"}
+
+@app.route("/api/v1.0/precipitation")
+def home():
+    return date
+
+list_ = {"stations"}
+
+@app.route("/api/v1.0/stations")
+def home():
+    return list_
+
+temperature = {"tobs"}
+
+@app.route("/api/v1.0/tobs")
+def home():
+    return temperature
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 #################################################
